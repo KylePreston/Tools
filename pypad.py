7e9b09085908eea0c345058e2771772febcb3adb
Kyle Preston <kylepreston.122@gmail.com>
2014-04-16 14:25:17 -0700

    Update pypad.py

--- pypad.py
+++ pypad.py
@@ -1,4 +1,4 @@
-# NOT READY FOR USE!!!!!!
+# NOT READY FOR USE!!!!
 import Tkinter as tk
 
 root = tk.Tk()
@@ -27,7 +27,8 @@ def find_action():
 	pass
 def selectAll_action():
 	pass
-# Define your icons
+
+# Define your image icons
 newicon = tk.PhotoImage(file='icons/newfile.gif')
 openicon = tk.PhotoImage(file='icons/open.gif')
 saveicon = tk.PhotoImage(file='icons/jesus.gif')
@@ -42,10 +43,8 @@ menuBar.add_cascade(label='PyPad')
 
 # File Menu
 fileMenu = tk.Menu(menuBar)
-# We've created the fileMenu and now it is a child of menuBar.
-# Now we can call it when we create a new hierarchical menu below.
-# using menuBar.add_cascade
-# fileMenu.add_command(label='New', accelerator='Ctrl+N', compound=tk.LEFT, image=newicon, underline=0)
+# Displays the column needed to see the add_commands
+menuBar.add_cascade(label='File', menu=fileMenu)
 fileMenu.add_command(label='New', accelerator='Command+N', compound=tk.LEFT,
 					 image=newicon, underline=0, command=new_file)
 fileMenu.add_command(label='Open', accelerator='Command+O', compound=tk.LEFT,
@@ -55,11 +54,10 @@ fileMenu.add_command(label='Save', accelerator='Command-S', compound=tk.LEFT,
 fileMenu.add_command(label='Save as', accelerator='Shift+Command+S', command=save_file_as)
 fileMenu.add_separator()
 fileMenu.add_command(label='Exit', accelerator='Command+W', command=exit_program)
-# This displays the actual column. Needed to see all the add_commands
-menuBar.add_cascade(label='File', menu=fileMenu)
 
 # Edit menu
 editMenu = tk.Menu(menuBar)
+menuBar.add_cascade(label='Edit', menu=editMenu)
 editMenu.add_command(label='Undo', accelerator='Command+Z', compound=tk.LEFT,
 					 image=undoicon, underline=0, command=undo_action)
 editMenu.add_command(label='Redo', accelerator='Shift-Command+S', compound=tk.LEFT,
@@ -71,15 +69,46 @@ editMenu.add_command(label='Copy', accelerator='Command-C', compound=tk.LEFT,
 editMenu.add_separator()
 editMenu.add_command(label='Find', accelerator='Command+F', command=find_action)
 editMenu.add_command(label='Select all', accelerator='Command+A', command=selectAll_action)
-menuBar.add_cascade(label='Edit', menu=editMenu)
 
 # View Menu
 viewMenu = tk.Menu(menuBar)
 menuBar.add_cascade(label='View', menu=viewMenu)
+# Capture and create showLine, an instance of class IntVar
+showLine = tk.IntVar()
+# Set the initial value
+showLine.set(1)
+viewMenu.add_checkbutton(label='Show line number', variable=showLine)
+showInBar = tk.IntVar()
+showInBar.set(1)
+viewMenu.add_checkbutton(label='Show info bar at bottom', variable = showInBar)
+highlight = tk.IntVar()
+viewMenu.add_checkbutton(label='Highlight current line', onvalue=1, offvalue=0, variable=highlight)
+# A Theme Menu cascaded inside the view menu
+themeMenu = tk.Menu(menuBar)
+viewMenu.add_cascade(label='Themes', menu=themeMenu)
+# Theme choices
+themeSelection = {
+'1. Default White': 'FFFFFF',
+'2. Greygarious Grey':'D1D4D1',
+'3. Lovely Lavender':'E1E1FF' , 
+'4. Aquamarine': 'D1E7E0',
+'5. Bold Beige': 'FFF0E1',
+'6. Cobalt Blue':'333AA',
+'7. Olive Green': '5B8340'
+}
+themeChoice = tk.StringVar()
+themeChoice.set('1. Default White')
+[themeMenu.add_radiobutton(label=i, variable=themeChoice) for i in sorted(themeSelection)]
 
+# About Menu
 aboutMenu = tk.Menu(menuBar)
 menuBar.add_cascade(label='About', menu=aboutMenu)
+aboutMenu.add_command(label='About')
+aboutMenu.add_command(label='Help')
 
+# Create the text box
+textPad = tk.Text(root)
+textPad.pack(expand=tk.YES, fill=tk.BOTH)
 # Display the menu bar
 root.config(menu=menuBar)
 root.mainloop()
